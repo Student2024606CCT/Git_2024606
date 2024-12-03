5c86742f8d5d829f47a69f8f47b361f88994dfed
Student2024606CCT <2024606@student.cct.ie>
2024-12-03 00:48:29 +0000

    added a simple print

--- src/git2024606/Git2024606.java
+++ src/git2024606/Git2024606.java
@@ -10,7 +10,7 @@ public class Git2024606 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Asafe - 2024606");
     }
     
 }
