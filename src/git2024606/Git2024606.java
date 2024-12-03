b2aaf6bdb1febfa2195548afcf9a31e64cb27e03
Student2024606CCT <2024606@student.cct.ie>
2024-12-03 00:05:30 +0000

    init

---
+++ src/git2024606/Git2024606.java
@@ -0,0 +1,16 @@
+package git2024606;
+
+/**
+ *
+ * @author asafeds
+ */
+public class Git2024606 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
